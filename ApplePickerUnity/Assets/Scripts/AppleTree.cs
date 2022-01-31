054d9b3b4d6ae80ef2a39cf04f030e087d63dd26
Campinator <joefakocamp@gmail.com>
2022-01-31 15:14:06 -0600

    Initial Creation
    Make Apple, AppleTree, Basket

---
+++ ApplePickerUnity/Assets/Scripts/AppleTree.cs
@@ -0,0 +1,54 @@
+/*
+ * Created By: Camp Steiner 
+ * Date Created: Jan 31, 2022
+ * 
+ * Last Edited By: N/A
+ * Last Edited: Jan 31, 2022
+ * 
+ * Description: Controls the movement of the Apple Tree
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in inspector")]
+    public float speed = 0.1f;
+    public float leftAndRightEdge = 10f;
+    public GameObject applePrefab;
+    public float secondsBetweenAppleDrops = 1f;
+    private long appleDelta = 0;
+    public float chanceToChangeDirections = 0.02f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement
+        transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        if(Mathf.Abs(transform.position.x) > leftAndRightEdge)
+        {
+            speed *= -1; //change position
+        }
+    }
+
+    // Called on fixed intervals (50 times per second)
+
+    private void FixedUpdate()
+    {
+        if(Random.value < chanceToChangeDirections)
+        {
+            speed *= -1; // random chance to change directions unprompted
+        }
+        if(appleDelta > secondsBetweenAppleDrops)
+        {
+
+        }
+    }
+}
